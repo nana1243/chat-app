f11bdc7edef03ea8723bce0598f6ef148a578d4e
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-12-18 12:15:46 +0900

    fea) hamburger 추가

---
+++ src/components/ui/hamburger/index.tsx
@@ -0,0 +1,18 @@
+import {Menu} from 'lucide-react';
+
+interface HamburgerProps {
+    size?: number;
+    color?: string;
+    strokeWidth?: number;
+}
+
+const Hamburger = ({size = 30, color = 'black', strokeWidth = 1.5}: HamburgerProps) => {
+
+    return (
+        <>
+            <Menu color={color} size={size} strokeWidth={strokeWidth}/>
+        </>
+    )
+}
+
+export default Hamburger;
