200bebe2d7e947735e16c665df884822632b5948
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-12-18 11:48:49 +0900

    fea) 프로젝트 셋업

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,7 @@ import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
-import Header from '../components/Header'
+import Header from '../components/Header/Header'
 
 export const Route = createRootRoute({
   component: () => (
