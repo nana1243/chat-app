a37e1cfb70844c5d4f7d8ca4d69ae674c302c239
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-12-20 11:16:40 +0900

    fea) Contents Header 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,40 @@
-import Hamburger from "@/components/ui/hamburger";
+import styles from './index.module.css';
+import {Bell, Plus} from 'lucide-react';
 
 export default function Header() {
 
     return (
         <>
-            <header>
+            <header className={styles.headerContainer}>
+                <div className={styles.headerTitle}>
+                    <h1 className="">Chat</h1>
+                </div>
+
+                <div className={styles.headerActions}>
+                    <button
+                        className={styles.addChatBtn}>
+                        <Plus size={18}/>
+                        <span className="font-medium">Chat</span>
+                    </button>
+
+                    <button className={styles.notificationBtn}>
+                        <Bell size={22}/>
+                    </button>
+
+
+                    {/* 프로필 섹션 */}
+                    <div className={styles.profileGroup}>
+                        <div className={styles.avatarWrapper}>
+                            <img
+                                src="src/assets/images/profile.png"
+                                alt="User"
+                                className={styles.avatarImage}
+                            />
+                        </div>
+                        <span className={styles.userName}>Leslie Alexander</span>
+                    </div>
+                </div>
+
             </header>
         </>
     )
